ec986f2e7292fdaacf3f0fe0456315853abd0d6c
Christian Häussler <c-n-i@web.de>
2018-06-18 16:27:36 +0200

    Shutdown servers gracefully
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- cmd/cdays/cdays.go
+++ cmd/cdays/cdays.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/cniweb/cdays/internal/routing"
 	"github.com/cniweb/cdays/internal/version"
@@ -25,13 +29,43 @@ func main() {
 		log.Fatal("The diagnostics port wasn't set")
 	}
 
+	var blServer, diagServer http.Server
+
 	go func() {
 		r := routing.NewBLRouter()
-		log.Fatal(http.ListenAndServe(":"+port, r))
+		blServer = http.Server{
+			Addr:    ":" + port,
+			Handler: r,
+		}
+		log.Fatal(blServer.ListenAndServe())
 	}()
 
-	{
+	go func() {
 		r := routing.NewDiagnosticsRouter()
-		log.Fatal(http.ListenAndServe(":"+diagPort, r))
+		diagServer = http.Server{
+			Addr:    ":" + diagPort,
+			Handler: r,
+		}
+		log.Fatal(diagServer.ListenAndServe())
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case killSignal := <-interrupt:
+		log.Printf("Got %s. Stopping...", killSignal)
+	}
+
+	{
+		ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFunc()
+		blServer.Shutdown(ctx)
+	}
+
+	{
+		ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFunc()
+		diagServer.Shutdown(ctx)
 	}
 }
